5e573d3bdd4d191b9eb01f6d8cb00d75b829b560
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-04-12 15:27:07 -0400

    c&p the given code

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -8,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class CrewComponent implements OnInit {
 
   crew: object[] = [
-    {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    { name: "Eileen Collins", firstMission: false },
+    { name: "Mae Jemison", firstMission: false },
+    { name: "Ellen Ochoa", firstMission: true }
   ];
 
+  memberBeingEdited: object = null;
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({ name: memberName, firstMission: isFirst });
+  }
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
 }
